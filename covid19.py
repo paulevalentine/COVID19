5c63096e49df46eefb1cb5872c51cc921585f401
paulevalentine <paul@the-valentines.co.uk>
2020-04-04 10:44:30 +0100

    General updates

--- covid19.py
+++ covid19.py
@@ -1,28 +1,28 @@
+"""Plot the COVID19 data for countries by population."""
+import urllib.request
 import numpy as np
 import pandas as pd
-import sys
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
-rcParams.update({'figure.autolayout': True})
-import urllib.request
 
-pd.set_option('display.expand_frame_repr', False)
+rcParams.update({'figure.autolayout': True})
+
 # get data from website and save to local location (datapath)
 url = 'https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-2020-04-03.xlsx'
 datapath = r'C:\Users\paul\Dropbox\Paul\COVID19\covid19Data.xlsx'
-file = urllib.request.urlretrieve(url,datapath)
+file = urllib.request.urlretrieve(url, datapath)
 
 # Get data from local file
 getData = pd.read_excel(datapath)
 # Set up a dataFrame and filter the data by country
 data = pd.DataFrame(getData)
 data.index = data['geoId']
-uk = data.filter(like='UK', axis = 0)
-us = data.filter(like='US', axis = 0)
-it = data.filter(like='IT', axis = 0)
-cn = data.filter(like='CN', axis = 0)
-es = data.filter(like='ES', axis = 0)
-fr = data.filter(like='FR', axis = 0)
+uk = data.filter(like='UK', axis=0)
+us = data.filter(like='US', axis=0)
+it = data.filter(like='IT', axis=0)
+cn = data.filter(like='CN', axis=0)
+es = data.filter(like='ES', axis=0)
+fr = data.filter(like='FR', axis=0)
 
 # Make numpy arrays of the data
 # UK Data
@@ -62,16 +62,16 @@ frz = np.array(fr['deaths'])
 frp = np.array(fr['popData2018'])
 
 # plot the data
-plt.figure(figsize=[5,5])
+plt.figure(figsize=[12, 5])
 
 # Deaths normalised by population
-plt.subplot(1,2,1)
-plt.plot(ukx,100*ukz/ukp, label = 'UK {}'.format(np.sum(ukz)))
-plt.plot(usx,100* usz/usp, label = 'US {}'.format(np.sum(usz)))
-plt.plot(itx, 100*itz/itp, label = 'IT {}'.format(np.sum(itz)))
-plt.plot(cnx,100*cnz/cnp, label = 'CN {}'.format(np.sum(cnz)))
-plt.plot(esx,100*esz/esp, label = 'ES {}'.format(np.sum(esz)))
-plt.plot(frx,100*frz/frp, label = 'FR {}'.format(np.sum(frz)))
+plt.subplot(1, 2, 1)
+plt.plot(ukx, 100*ukz/ukp, label='UK {}'.format(np.sum(ukz)))
+plt.plot(usx, 100 * usz/usp, label='US {}'.format(np.sum(usz)))
+plt.plot(itx, 100*itz/itp, label='IT {}'.format(np.sum(itz)))
+plt.plot(cnx, 100*cnz/cnp, label='CN {}'.format(np.sum(cnz)))
+plt.plot(esx, 100*esz/esp, label='ES {}'.format(np.sum(esz)))
+plt.plot(frx, 100*frz/frp, label='FR {}'.format(np.sum(frz)))
 plt.xticks(rotation=45)
 plt.grid()
 plt.xlabel('Date')
@@ -80,19 +80,19 @@ plt.legend()
 plt.title('Deaths from COVID 19 normalised by population')
 
 # Cases normalised by population
-plt.subplot(1,2,2)
-plt.plot(ukx,100*uky/ukp, label = 'UK {}'.format(np.sum(uky)))
-plt.plot(usx, 100*usy/usp, label = 'US {}'.format(np.sum(usy)))
-plt.plot(itx, 100*ity/itp, label = 'IT {}'.format(np.sum(ity)))
-plt.plot(cnx,100*cny/cnp, label = 'CN {}'.format(np.sum(cny)))
-plt.plot(esx,100*esy/esp, label = 'ES {}'.format(np.sum(esy)))
-plt.plot(frx,100*fry/frp, label = 'FR {}'.format(np.sum(fry)))
+plt.subplot(1, 2, 2)
+plt.plot(ukx, 100*uky/ukp, label='UK {}'.format(np.sum(uky)))
+plt.plot(usx, 100*usy/usp, label='US {}'.format(np.sum(usy)))
+plt.plot(itx, 100*ity/itp, label='IT {}'.format(np.sum(ity)))
+plt.plot(cnx, 100*cny/cnp, label='CN {}'.format(np.sum(cny)))
+plt.plot(esx, 100*esy/esp, label='ES {}'.format(np.sum(esy)))
+plt.plot(frx, 100*fry/frp, label='FR {}'.format(np.sum(fry)))
 plt.xticks(rotation=45)
 plt.grid()
 plt.xlabel('Date')
 plt.ylabel('% of population per day')
 plt.legend()
 plt.title('Cases of COVID 19 normalised by population')
-mng = plt.get_current_fig_manager()
-mng.window.state('zoomed')
+# mng = plt.get_current_fig_manager()
+# mng.window.state('zoomed')
 plt.show()
--- ukcovi19.py
+++ ukcovi19.py
@@ -1,15 +1,8 @@
-# analysis of just UK Cases
+"""Print UK COVID19 data."""
 import matplotlib.pyplot as plt
-import matplotlib.dates
-import numpy as np
 import pandas as pd
-import os
 import datetime as dt
-from matplotlib import style
-
-
-# make the directory current
-# os.chdir(r'C:\Users\paul\Dropbox\Paul\COVID19')
+plt.rcParams['figure.figsize'] = [12, 5]
 
 # get the data from a downloaded excel file
 data = pd.read_excel(r'C:\Users\paul\Dropbox\Paul\COVID19\covid19Data.xlsx')
@@ -21,14 +14,14 @@ data.index = data['geoId']
 uk_data = data.loc['UK', :]
 
 # plot the Cases
-x=dt.datetime.strptime('2020/03/23', '%Y/%m/%d').date()
-y = dt.datetime.strptime('2020/04/06','%Y/%m/%d').date()
-ax1 = plt.subplot2grid((1,2), (0,0), rowspan=1, colspan=1)
-ax2 = plt.subplot2grid((1,2), (0,1), rowspan=1, colspan=1)
+x = dt.datetime.strptime('2020/03/23', '%Y/%m/%d').date()
+y = dt.datetime.strptime('2020/04/06', '%Y/%m/%d').date()
+ax1 = plt.subplot2grid((1, 2), (0, 1), rowspan=1, colspan=1)
+ax2 = plt.subplot2grid((1, 2), (0, 0), rowspan=1, colspan=1)
 ax1.plot(uk_data['dateRep'], uk_data['cases'], 'red')
 plt.sca(ax1)
-plt.axvline(x,0,2000)
-plt.axvline(y,0,2000)
+plt.axvline(x, 0, 2000)
+plt.axvline(y, 0, 2000)
 plt.grid()
 plt.xlabel('Date')
 plt.ylabel('Number per day')
@@ -36,14 +29,14 @@ plt.title('COVID19 Cases')
 plt.xticks(rotation=45)
 ax2.plot(uk_data['dateRep'], uk_data['deaths'], 'red')
 plt.sca(ax2)
-plt.axvline(x,0,2000)
-plt.axvline(y,0,2000)
+plt.axvline(x, 0, 2000)
+plt.axvline(y, 0, 2000)
 plt.grid()
 plt.xlabel('Date')
 plt.ylabel('Number per day')
 plt.title('COVID19 Deaths')
 plt.xticks(rotation=45)
-mng = plt.get_current_fig_manager()
-mng.window.state('zoomed')
+# mng = plt.get_current_fig_manager()
+# mng.window.state('zoomed')
 plt.tight_layout()
 plt.show()
